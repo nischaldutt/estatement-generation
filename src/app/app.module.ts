a90ba0cf5d163b8ffe57d665b6e2416fc1f98b55
nischaldutt <nischaldutt01@gmail.com>
2022-06-19 18:37:18 +0530

    update: connecting statement module to app component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StatementGenerationModule } from './statement-generation/statement-generation.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    StatementGenerationModule
   ],
   providers: [],
   bootstrap: [AppComponent]
