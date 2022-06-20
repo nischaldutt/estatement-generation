2b1c206b6951614f7b7b6ba38777a31491d5e91f
nischaldutt <nischaldutt01@gmail.com>
2022-06-20 22:47:58 +0530

    feat: adding radio button group and fixing styles

--- src/app/shared/date.validator.ts
+++ src/app/shared/date.validator.ts
@@ -2,7 +2,7 @@ import { AbstractControl } from '@angular/forms';
 
 export function ValidateDate(control: AbstractControl) {
   const regex = /([12]\d{3}-(0[1-9]|1[0-2]|[1-9])-(0[1-9]|[12]\d|3[01]|[1-9]))/;
-  const { day, month, year } = control.value;
+  const { day, month, year } = control?.value;
   const dateEntered = `${year}-${month}-${day}`
 
   return regex.test(dateEntered)? null: { invalidDate: true }
