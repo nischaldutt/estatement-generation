f34d2f193e939d776d0fc29c4c99b3612dc88717
nischaldutt <nischaldutt01@gmail.com>
2022-06-23 07:40:14 +0530

    fix: fixing routing with module imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginModule } from './login/login.module';
 import { StatementGenerationModule } from './statement-generation/statement-generation.module';
+import { ViewStatementModule } from './view-statement/view-statement.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    LoginModule,
     StatementGenerationModule,
+    ViewStatementModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
