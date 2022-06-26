c55c582e385df51f67ae1e4006a0d04e112bc41f
nischaldutt <nischaldutt01@gmail.com>
2022-06-26 10:23:50 +0530

    feat: creating login form

--- src/app/modules/login/login/login.component.ts
+++ src/app/modules/login/login/login.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,7 +13,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
-  constructor() {}
+  loginForm!: FormGroup;
 
-  ngOnInit(): void {}
+  constructor(private formBuilder: FormBuilder, private router: Router) {}
+
+  ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required]),
+    });
+  }
+
+  get email() {
+    return this.loginForm.get('email');
+  }
+
+  get password() {
+    return this.loginForm.get('password');
+  }
+
+  handleLogin() {
+    console.log({ form: this.loginForm });
+    if (this.loginForm.valid) {
+      console.log({ test: this.loginForm.value });
+    }
+    this.router.navigateByUrl('/generate-statement');
+  }
 }
