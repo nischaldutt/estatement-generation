1a74c5e4f4b5cf22f9fade916705cb6e6708f52b
nischaldutt <nischaldutt01@gmail.com>
2022-06-26 10:22:37 +0530

    update: moving pipe to shared module

--- src/app/shared/pipes/first-error-key.pipe.ts
+++ src/app/shared/pipes/first-error-key.pipe.ts
@@ -1,14 +1,12 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-  name: 'firstErrorKey'
+  name: 'firstErrorKey',
 })
 export class FirstErrorKeyPipe implements PipeTransform {
-
   transform(error: any, ...args: unknown[]): unknown {
     // console.log({error})
     const keys = Object.keys(error);
-    return keys && keys.length>0 ? keys[0] : null;
+    return keys && keys.length > 0 ? keys[0] : null;
   }
-
 }
