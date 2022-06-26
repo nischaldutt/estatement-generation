817b45d3a82d0b5c973fe784f896de4c5ebf62fb
nischaldutt <nischaldutt01@gmail.com>
2022-06-26 16:49:23 +0530

    update: adding login form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
-
+import { Component, HostListener } from '@angular/core';
+import { Router } from '@angular/router';
+import { PlatformLocation } from '@angular/common';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'estatement-generation';
+
+  constructor(private router: Router, private _location: PlatformLocation) {}
+  @HostListener('window:popstate', ['$event'])
+  onPopState(event: Event) {
+    // event.stopPropagation();
+    // event.preventDefault();
+  }
 }
